a93e3b6da0da1848fb590be287b530b53f73761f
Summy Singh <summysingh86@gmail.com>
2021-07-12 22:23:38 +0530

    added

---
+++ C-Sharp/68_PrivateConstructor/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _68_PrivateConstructor
+{
+    public class PrivCons
+    {
+        private PrivCons()
+        {
+            Console.WriteLine("Private Constructor");
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //inaccsibble due to access level
+            PrivCons con = new PrivCons();
+        }
+    }
+}
---
+++ C-Sharp/69_PrivateConstructor1/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _69_PrivateConstructor1
+{
+    public class PrivCons
+    {
+        public static string name;
+        public static int age;
+
+        private PrivCons()
+        {
+            Console.WriteLine("Private Constructor");
+        }
+
+        public PrivCons(string _name,int _age)
+        {
+            name = _name;
+            age = _age;
+
+            Console.WriteLine("Called from Another Constructor Where Name:{0}, Age:{1}", name, age);
+        }
+
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //in this case private constructor won't be called
+            PrivCons cons = new PrivCons("Summy", 25);
+
+            PrivCons cons1 = new PrivCons("Sunny", 28);
+
+            Console.ReadLine();
+        }
+    }
+}
